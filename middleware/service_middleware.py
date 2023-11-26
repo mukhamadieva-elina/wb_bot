77d75487983deccbf2ed8367d5d47076b011ba25
John Doe <solodovnikov.sf@edu.spbstu.ru>
2023-11-26 17:48:16 +0300

    fix 8 issue

---
+++ middleware/service_middleware.py
@@ -0,0 +1,37 @@
+from typing import Callable, Any, Dict, Awaitable
+
+from aiogram import BaseMiddleware
+from aiogram.types import Message
+
+from db.product_service import ProductService
+from db.user_service import UserService
+
+
+class CounterMiddleware(BaseMiddleware):
+    def __init__(self) -> None:
+        self.counter = 0
+
+    async def __call__(
+        self,
+        handler: Callable[[Message, Dict[str, Any]], Awaitable[Any]],
+        event: Message,
+        data: Dict[str, Any]
+    ) -> Any:
+        self.counter += 1
+        data['counter'] = self.counter
+        return await handler(event, data)
+
+class ServiceMiddleware(BaseMiddleware):
+    def __init__(self, engine) -> None:
+        self.product_service = ProductService(engine)
+        self.user_service = UserService(engine)
+
+    async def __call__(
+        self,
+        handler: Callable[[Message, Dict[str, Any]], Awaitable[Any]],
+        event: Message,
+        data: Dict[str, Any]
+    ) -> Any:
+        data['product_service'] = self.product_service
+        data['user_service'] = self.user_service
+        return await handler(event, data)
