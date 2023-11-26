8fc44674448cdf23dda42e16c70233f6dc1742c9
Elina <31386525+mukhamadieva-elina@users.noreply.github.com>
2023-11-26 22:33:54 +0300

    Change structure of items checker and notifier.

--- services/items_checker.py
+++ services/items_checker.py
@@ -1,24 +1,12 @@
-from notifier import Notifier
-from typing import TypedDict
+import time
 
-class Item(TypedDict):
-    art: str
-    last_price: int
+async def check_price_change(item_art):
+    pass
+
+async def check_availability(item_art):
+    pass
 
 
-class ItemsChecker():
-    def __int__(self, notifier: Notifier):
-        self.items : list[Item] = []
-        self.notifier = notifier
-
-    def check_items(self, freq=60*5):
-        for item in self.items:
-            item_art, last_price = item["art"], item["last_val"]
-            new_price = check_price_change(item_art, last_price)
-            if new_price:
-                self.notifier.notify(item_art, new_price)
-
-    def update(self):
-        pass
-
+def update(engine, session, freq=60 * 5):
+    time.sleep(freq)
 
