692d77f8f2b9e453a616b4e22d5c75b09b8ad829
lupperant <nefedovatata01@gmail.com>
2023-11-27 00:46:51 +0300

    change notifier first version

--- services/items_checker.py
+++ services/items_checker.py
@@ -1,12 +1,46 @@
+from api.api_service import get_product
+
+from db.dto.ProductUpdateDto import ProductUpdateDto
+from db.product_service import ProductService
+
 import time
 
+from services import notifier
+
+
 async def check_price_change(item_art):
-    pass
+    history = await get_product(item_art)
+    if history:
+        price = history[0]['salePriceU'] / 100
+        return price
+
 
 async def check_availability(item_art):
-    pass
+    history = await get_product(item_art)
+    if history:
+        availability = False
+        for size in history[0]['sizes']:
+            if len(size['stocks']):
+                availability = True
+                break
+        return availability
 
 
-def update(engine, session, freq=60 * 5):
-    time.sleep(freq)
+async def update(message, product_service: ProductService, freq=60):
+    while True:
+        products_from_bd = product_service.get_all_product()  ##продукты из бд
+        for product in products_from_bd:
+            product_art, product_price, product_avail = product.number, product.price, product.availability
+            availability_from_api = await check_availability(product_art)
+            price_from_api = await check_price_change(product_art)
+            if product_price != price_from_api:
+                new_product = ProductUpdateDto(price=price_from_api)
+                product_service.patch_product(product_art, new_product)
+                notifier.notify(product_art, price_from_api, product_service, message)
+            if product_avail != availability_from_api:
+                new_product = ProductUpdateDto(availability=availability_from_api)
+                product_service.patch_product(product_art, new_product)
+                notifier.notify_avail(product_art, availability_from_api, product_service, message)
+        time.sleep(freq)
+        print("updating")
 
--- services/notifier.py
+++ services/notifier.py
@@ -1,11 +1,22 @@
-from handlers import router
+from db.product_service import ProductService
+from aiogram import Bot
 
 
-@router.message()
-def notify():
-    pass
+def notify(item_art, new_val: float, product_service: ProductService, bot: Bot):
+    positions = product_service.get_users_of_product(item_art)
+    for item in positions:
+        if item.start_price:
+            if item.start_price > new_val:
+                bot.send_message(item.user_telegram_id, "Цена понизилась")
+            elif item.start_price < new_val:
+                bot.send_message(item.user_telegram_id, "Цена повысилась")
 
 
-@router.message()
-def notify():
-    pass
+def notify_avail(item_art, new_availability, product_service: ProductService, bot: Bot):
+    positions = product_service.get_users_of_product(item_art)
+    for item in positions:
+        if item:
+            if new_availability:
+                bot.send_message(item.user_telegram_id, "Товар появился в наличии")
+            else:
+                bot.send_message(item.user_telegram_id, "Товар не в наличии")
