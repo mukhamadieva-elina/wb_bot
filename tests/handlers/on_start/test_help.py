059d543810945caee2c99a3d1f722bd020f4e646
pletn <pletnyovski@gmail.com>
2024-02-16 02:07:34 +0300

    add items checker tests

--- tests/handlers/on_start/test_help.py
+++ tests/handlers/on_start/test_help.py
@@ -9,9 +9,10 @@ from tests.conftest import input_message
 from handlers.on_start.help import help
 from tests.conftest import *
 
+
 @pytest.mark.asyncio
 async def test_help(input_message: aiogram.types.Message, mocker: MockFixture):
-    mock : AsyncMock = mocker.patch("aiogram.types.Message.answer", new_callable=AsyncMock)
+    mock: AsyncMock = mocker.patch("aiogram.types.Message.answer", new_callable=AsyncMock)
 
     await help(input_message, None)
 
--- tests/test_services/test_items_checker.py
+++ tests/test_services/test_items_checker.py
@@ -1,6 +1,9 @@
+from unittest.mock import patch
+
 import pytest
 
-from services.items_checker import get_price_product_api, get_aval_product_api
+from db.product_service import ProductService
+from services.items_checker import get_price_product_api, get_aval_product_api, get_changed_items
 
 
 @pytest.mark.parametrize("api_obj, expected_price", [
@@ -28,3 +31,58 @@ def test_get_price_product_api(api_obj, expected_price):
 ])
 def test_get_aval_product_api(api_obj, expected_avail):
     assert get_aval_product_api(api_obj) == expected_avail
+
+
+@pytest.mark.asyncio
+async def test_get_changed_items_no_changes(product_service: ProductService, product_example):
+    product_service.get_all_product.return_value = [product_example]
+    with patch('api.api_service.get_product') as mock_get_product:
+        mock_get_product.return_value = [
+            {"id": 197659450, "name": "Cолнцезащитные очки",
+             "priceU": 199000, "salePriceU": 42000, "sizes": [
+                {"name": "", "origName": "0", "rank": 0, "optionId": 320451004, "returnCost": 0,
+                 "stocks": [{'ww': 'aa'}]}]}]
+        aval_changed_items, price_changed_items = await get_changed_items(product_service)
+    assert aval_changed_items == []
+    assert price_changed_items == []
+
+
+@pytest.mark.asyncio
+async def test_get_changed_items_aval_change(product_service: ProductService, product_example):
+    product_service.get_all_product.return_value = [product_example]
+    with patch('api.api_service.get_product') as mock_get_product:
+        mock_get_product.return_value = [
+            {"id": 197659450, "name": "Cолнцезащитные очки",
+             "priceU": 199000, "salePriceU": 42000, "sizes": [
+                {"name": "", "origName": "0", "rank": 0, "optionId": 320451004, "returnCost": 0,
+                 "stocks": []}]}]
+        aval_changed_items, price_changed_items = await get_changed_items(product_service)
+    assert aval_changed_items == [(product_example.Product.number, False, -1)]
+    assert price_changed_items == []
+
+    product_example.Product.availability = False
+    product_example.Product.price = -1
+    with patch('api.api_service.get_product') as mock_get_product:
+        mock_get_product.return_value = [
+            {"id": 197659450, "name": "Cолнцезащитные очки",
+             "priceU": 199000, "salePriceU": 42000, "sizes": [
+                {"name": "", "origName": "0", "rank": 0, "optionId": 320451004, "returnCost": 0,
+                 "stocks": [{'ww': 'qq'}]}]}]
+        aval_changed_items, price_changed_items = await get_changed_items(product_service)
+    assert aval_changed_items == [(product_example.Product.number, True, 420.0)]
+    assert price_changed_items == []
+
+
+
+@pytest.mark.asyncio
+async def test_get_changed_items_price_change(product_service: ProductService, product_example):
+    product_service.get_all_product.return_value = [product_example]
+    with patch('api.api_service.get_product') as mock_get_product:
+        mock_get_product.return_value = [
+            {"id": 197659450, "name": "Cолнцезащитные очки",
+             "priceU": 199000, "salePriceU": 52000, "sizes": [
+                {"name": "", "origName": "0", "rank": 0, "optionId": 320451004, "returnCost": 0,
+                 "stocks": [{'aa': 'qq'}]}]}]
+        aval_changed_items, price_changed_items = await get_changed_items(product_service)
+    assert aval_changed_items == []
+    assert price_changed_items == [(product_example.Product.number, 520.0)]
